9a26d374e4208b80a02e6d4680efae6607bbd63c
Floriane-MAFFEI <floriane.oclock@gmail.com>
2023-06-18 19:39:44 +0200

    Add All Portefolio Project

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Récupération de l'élément bandeau
+var bandeau = document.getElementById('bandeau');
+
+// Récupération de la largeur du texte
+var largeurTexte = bandeau.offsetWidth;
+
+// Définition de la largeur de l'élément bandeau
+bandeau.style.width = largeurTexte + 'px';
+
+
