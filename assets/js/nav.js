5e8ae12971553ebbaa25b4e56b0b30fe545ba406
Floriane-MAFFEI <floriane.oclock@gmail.com>
2024-01-12 14:28:35 +0100

    setting up a basic image with link to new portfolio

--- assets/js/nav.js
+++
@@ -1,7 +0,0 @@
-const mobileNavToggle = document.querySelector('.mobile-nav-toggle'); // Sélectionne le bouton de navigation mobile
-const header = document.querySelector('#header'); // Sélectionne l'élément header
-
-mobileNavToggle.addEventListener('click', () => { // Ajoute un écouteur d'événement pour le clic sur le bouton de navigation mobile
-    header.classList.toggle('mobile-nav-active'); // Ajoute ou supprime la classe 'mobile-nav-active' à l'élément
-    // header lors du clic
-});
